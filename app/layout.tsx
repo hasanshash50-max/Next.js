878bd42c266f4af72fbc3aec3ddabddf4e1bd67b
hasanshash50 <hasanshash50@gmail.com>
2026-09-02 16:03:26 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
              style={{
                margin:"5px 8px 0px 0px",
                display:"flex",
-               gap:"5px",
+               gap:"30px",
                color:"#fff"
              }}>
                 <Link href="/">About</Link>
