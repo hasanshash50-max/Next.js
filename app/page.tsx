eda424adf353dbe9eb1306b19ba3afd652a644a4
hasanshash50 <hasanshash50@gmail.com>
2026-09-02 15:59:22 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,13 @@ export default function()
                 top:"160px",
                 left:"17px",
                 fontWeight:"620",
+                color:"#000"
             }}>About Me</h1><br></br>
             <p style={{
                 position:"absolute",
                 top:"200px",
-                left:"17px"
+                left:"17px",
+                color:"#000"
             }}>Hi , I'm a possionate web developer with expertise in Next.js , React <br></br>
                 and Tailwind CSS .I love building modern , responsive ,
                 and user-friendly websites.
